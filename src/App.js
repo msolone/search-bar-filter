b3186a2d203b2439cce2e3e24d4c3ea9a090e3b7
Michael Solone <michaelsolone@gmail.com>
2018-08-31 22:42:24 -0400

    Created json and display list of names that is filtered by search input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Data from "./Data/names.json";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: ""
+    };
+  }
+
+  componentDidMount() {
+    console.log(Data.person);
+  }
+
+  updateSearch(event) {
+    this.setState({
+      search: event.target.value
+    });
+  }
   render() {
+    let filteredNames = Data.people.filter(person => {
+      return person.name.toLowerCase().includes(this.state.search.toLocaleLowerCase());
+    });
     return (
-      <section>
-        HELLO WORLD
+      <section className="main">
+        <input
+          type="text"
+          value={this.state.search}
+          onChange={this.updateSearch.bind(this)}
+        />
+        <ul>
+          {filteredNames.map((dude, i) => {
+            return <li key={i}>{dude.name}</li>;
+          })}
+        </ul>
       </section>
     );
   }
